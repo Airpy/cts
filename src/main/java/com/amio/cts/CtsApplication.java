f2d58f704be1ea551bcc212ee0e364db6ef6feba
Airpy <getimer@gmail.com>
2018-10-20 21:18:07 +0800

    初始化自动化项目

---
+++ src/main/java/com/amio/cts/CtsApplication.java
@@ -0,0 +1,12 @@
+package com.amio.cts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CtsApplication.class, args);
+    }
+}
